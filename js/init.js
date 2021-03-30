d43bc7aba3ceb2e59eb72ac523a2e9af93309688
ProyectofinalKusn <81657270+ProyectofinalKusn@users.noreply.github.com>
2021-03-30 17:27:08 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyAzeoou32aO1gKXDyVlqGnMWBCbRIgSFaY",
+    authDomain: "proyectofinalkus.firebaseapp.com",
+    projectId: "proyectofinalkus",
+    storageBucket: "proyectofinalkus.appspot.com",
+    messagingSenderId: "908584422900",
+    appId: "1:908584422900:web:d13f0a9bcb4cc0f2ecfda1",
+    measurementId: "G-896K580T99"
 });
